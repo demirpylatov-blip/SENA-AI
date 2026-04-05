89fc98dda226fb4299b434c43e91665b18726f04
demirpylatov-blip <demirpylatov@gmail.com>
2026-04-05 15:14:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -481,14 +481,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
- def main():
-    app = Application.builder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
