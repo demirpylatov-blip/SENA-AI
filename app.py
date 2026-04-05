48a16517fe887f661cb3e66f6bf36180df2526cb
demirpylatov-blip <demirpylatov@gmail.com>
2026-04-05 15:50:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT, chat))
 
-    app.run_polling()
+  app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
